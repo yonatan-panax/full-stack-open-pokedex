d00d5be99f1fcd360c97a5adc66bfcd25d7f929f
yonatan-panax <yonatan@thepanax.com>
2024-05-17 16:31:19 +0300

    commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8090;
 
 app.use(express.static("dist"));
 
